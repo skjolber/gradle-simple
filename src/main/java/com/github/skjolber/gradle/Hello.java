7105dd8c660b57ffffec0214f15518bf070cad69
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2018-05-02 13:48:19 +0200

    various changes

--- src/main/java/com/github/skjolber/gradle/Hello.java
+++ src/main/java/com/github/skjolber/gradle/Hello.java
@@ -1,10 +1,11 @@
+package com.github.skjolber.gradle;
 
 public class Hello {
   
   public static String GREETING = "Hello world!";
   
   public static void main(String []args) {
-    
+    System.out.println(GREETING);
   }  
 }
 
