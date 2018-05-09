4ea085c4d22b09d1bf5c8ea8c57c9295b7e65101
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2018-05-09 14:13:40 +0200

    add pom

--- src/main/java/com/github/skjolber/gradle/Hello.java
+++ src/main/java/com/github/skjolber/gradle/Hello.java
@@ -2,9 +2,7 @@ package com.github.skjolber.gradle;
 
 public class Hello {
 
-
 	public static String GREETING = "Hello world!";
-
 
 	public static void main(String []args) {
 		System.out.println(GREETING);
